074dbae521560da740084857fd3f1a2b207a91a0
田中陽介 <tanaka@n2i.jp>
2022-09-07 18:30:17 +0900

    setting health

--- routes/health.js
+++ routes/health.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('version:1.0.0');
+  res.status(200).json({success: true, message: 'version:1.0.0'});
 });
 
 module.exports = router;
